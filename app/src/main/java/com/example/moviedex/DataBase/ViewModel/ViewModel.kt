f697efffda3e875114b8874ddc4c1cb5319f54cb
hscanales <00136317@uca.edu.sv>
2019-06-11 08:09:59 -0600

    fixed retrofit and viewmodel

--- app/src/main/java/com/example/moviedex/DataBase/ViewModel/ViewModel.kt
+++ app/src/main/java/com/example/moviedex/DataBase/ViewModel/ViewModel.kt
@@ -4,70 +4,84 @@ import android.app.Application
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.MutableLiveData
 import com.example.moviedex.DataBase.DAO.previewDao
 import com.example.moviedex.DataBase.Entities.Movie
 import com.example.moviedex.DataBase.Entities.MoviePreview
 import com.example.moviedex.DataBase.Network.Network
 import com.example.moviedex.DataBase.Repository.movieRepo
 import com.example.moviedex.DataBase.Roomie
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ViewModel(private val app : Application): AndroidViewModel(app) {
 
     private val repository: movieRepo
+    private val scope = CoroutineScope(Dispatchers.IO)
 
-    init{
+    init {
         val movieoDAO = Roomie.getInstance(app).movieDao()
         val previeDAO = Roomie.getInstance(app).previewDao()
-        repository = movieRepo(movieoDAO,previeDAO, Network.getNetworkInstance())
+        repository = movieRepo(movieoDAO, previeDAO, Network.getNetworkInstance())
     }
+
+    private val movieslist = MutableLiveData<MutableList<MoviePreview>>()
+
+    private val movieResult = MutableLiveData<Movie>()
+
 
     private suspend fun insertpreview(preview: MoviePreview) = repository.insertPreview(preview)
 
     fun todospreview(): LiveData<List<MoviePreview>> = repository.todosPreview()
 
-    private suspend  fun nukepreview() = repository.nukePreview()
+    private suspend fun nukepreview() = repository.nukePreview()
 
 
-    fun search(search:String) = viewModelScope.launch {
-        this@ViewModel.nukepreview()
-        val response = repository.getPreview(search).await()
-
-        if(response.isSuccessful) with(response){
-            this.body()?.Search.forEach { this@ViewModel.insertpreview(it) }
-        }else with(response){
-            when(response.code()){
-                404 -> {
-                    Toast.makeText(app,"Oh shxt", Toast.LENGTH_LONG).show()
+    fun search(search: String) =
+        scope.launch {
+            val response = repository.getPreview(search).await()
+            if (response.isSuccessful) {
+                when (response.code()) {
+                    200 -> movieslist.postValue(
+                        response.body()?.Search?.toMutableList() ?: arrayListOf(
+                            MoviePreview(
+                                Title = "Dummy 1"
+                            ), MoviePreview(Title = "Dummy 2")
+                        )
+                    )
                 }
+            } else {
+                Toast.makeText(app, "Ocurrio un error", Toast.LENGTH_LONG).show()
             }
         }
-    }
+
 
     private suspend fun insertMain(preview: Movie) = repository.insertMain(preview)
 
     fun todosMain(): LiveData<List<Movie>> = repository.todosMain()
 
-    private suspend  fun nukeMain() = repository.nukeMain()
+    private suspend fun nukeMain() = repository.nukeMain()
 
 
-    fun title(search:String) = viewModelScope.launch {
-        this@ViewModel.nukepreview()
-        val response = repository.getMain(search).await()
-
-        if(response.isSuccessful) with(response){
-            this.body().also{ this@ViewModel.insertMain(it!!) }
-
-
-
-        }else with(response){
-            when(response.code()){
-                404 -> {
-                    Toast.makeText(app,"Oh shxt", Toast.LENGTH_LONG).show()
+    fun title(name: String) {
+        scope.launch {
+            val response = repository.getMain(name).await()
+            if (response.isSuccessful) with(response) {
+                when (this.code()) {
+                    200 -> movieResult.postValue(this.body())
                 }
+            } else {
+                Toast.makeText(app, "Ocurrio un error", Toast.LENGTH_LONG).show()
             }
         }
+
+
     }
+
+
+    fun getPreviews(): LiveData<MutableList<MoviePreview>> = movieslist
+
+    fun getMains(): LiveData<Movie> = movieResult
 
 }
